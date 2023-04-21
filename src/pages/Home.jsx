504d5f28af2d20da1edcc7808cba6c6f2b62b374
Agyanim <71318355+Agyanim@users.noreply.github.com>
2023-04-21 17:29:05 +0000

    destination page added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,11 @@ const Home = () => {
     <>
       <div className=" home-container absolute w-[100svw] h-screen">
         <section className="flex  justify-center items-center w-full relative mt-[22rem] lg:mt-[26rem]">
-          <div className="flex flex-col gap-[2rem] lg:gap-0 lg:flex-row text-white absolute w-[95%] lg:w-[80%] justify-between items-center pb-[5%] top-[-15rem]">
-            <section className="flex flex-col justify-center items-center  lg:items-start lg:justify-start w-[100%] lg:w-1/2 p-2">
+          <div className="flex flex-col gap-[2rem] lg:gap-0 lg:flex-row text-white absolute w-[95%] lg:w-[80%] justify-between items-center pb-[5%] top-[-18rem]">
+            <section className="flex flex-col justify-center items-center  lg:items-start lg:justify-start w-[100%] lg:w-1/2 p-2 mt-4">
               <h5 className="text-primaryGray ">SO, YOU WANT TO TRAVEL TO</h5>
               <h1>SPACE</h1>
-              <p className="text-base font-Barlow text-center lg:text-left lg:text-lg px-4 lg:px-0 text-primaryGray w-[25rem]">
+              <p className="text-base font-Barlow text-center lg:text-left lg:text-lg px-4 lg:px-0 text-primaryGray w-[28rem]">
                 Let’s face it; if you want to go to space, you might as well
                 genuinely go to outer space and not hover kind of on the edge of
                 it. Well sit back, and relax because we’ll give you a truly out
