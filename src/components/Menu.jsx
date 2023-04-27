b09d57447fe6ad11cdb18f46e53e4ed9211185c4
agyanim <agyanimconsultancy@gmail.com>
2023-04-27 16:41:21 +0000

    mobile view

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -41,7 +41,7 @@ const Menu = () => {
       });
     
   return (
-    <div className='absolute translate-x-[15rem] translate-y-[5rem]'>{renderLink}</div>
+    <div className='fixed translate-x-[15rem] translate-y-[4rem]'>{renderLink}</div>
   )
 }
 
